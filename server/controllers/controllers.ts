2057cbefba5734d6d7027f123f076bbc7c1c89ac
Jonathan Dump <jbrodii77@gmail.com>
2023-08-22 16:36:41 +0300

    (back) Make compare route

--- server/controllers/controllers.ts
+++ server/controllers/controllers.ts
@@ -2,16 +2,28 @@ const asyncHandler = require("express-async-handler");
 const Character = require("../models/character");
 import express, { Request, Response } from "express";
 import { Character } from "../interfaces/interfaces";
-import { log } from "console";
 
-exports.setCharacters = asyncHandler(async (req: Request, res: Response) => {
-  req.body.characters.forEach(async (character: Character) => {
-    console.log(character.coords);
+// exports.setCharacters = asyncHandler(async (req: Request, res: Response) => {
+//   req.body.characters.forEach(async (character: Character) => {
+//     console.log(character.coords);
 
-    const char = new Character(character);
-    console.log(char);
+//     const char = new Character(character);
+//     console.log(char);
 
-    await char.save();
-  });
-  res.json({ msg: "chars created" });
+//     await char.save();
+//   });
+//   res.json({ msg: "chars created" });
+// });
+
+exports.compare = asyncHandler(async (req: Request, res: Response) => {
+  const character = await Character.findOne({ name: req.body.name });
+  const [x, y] = req.body.coords;
+  const [startX, startY] = character.coords[0];
+  const [endX, endY] = character.coords[1];
+
+  if (x > endX || x < startX || y > endY || y < startY) {
+    res.json({ message: "wrong" });
+  } else {
+    res.json({ message: "correct" });
+  }
 });
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response } from "express";
 const router = express.Router();
 const controllers = require("../controllers/controllers");
+
 router.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server11");
 });
 
-router.post("/create", controllers.setCharacters);
+// router.post("/create", controllers.setCharacters);
+// module.exports = router;
+
+router.get("/compare", controllers.compare);
+
 module.exports = router;
